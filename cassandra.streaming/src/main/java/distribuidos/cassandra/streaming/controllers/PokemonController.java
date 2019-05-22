3d896aee11343d6f7e81f97a04126bc8a8002bd7
Diego Mellis <diego.mellis@usach.cl>
2019-05-21 22:02:33 -0400

    CHG: Method to count pokemones created

--- cassandra.streaming/src/main/java/distribuidos/cassandra/streaming/controllers/PokemonController.java
+++ cassandra.streaming/src/main/java/distribuidos/cassandra/streaming/controllers/PokemonController.java
@@ -37,5 +37,10 @@ public class PokemonController {
         return ResponseEntity.ok(pokemonService.getPokemonesByName(name));
     }
 
+    @GetMapping("/count")
+    public ResponseEntity<Long> countPokemones()
+    {
+        return ResponseEntity.ok(pokemonService.countPokemons());
+    }
 
 }
