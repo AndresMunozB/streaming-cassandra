3e848dc539bbf6e7c968e18ecc6411cee379456f
Diego Mellis <diego.mellis@usach.cl>
2019-05-21 22:04:28 -0400

    CHG: CrossOrigin added

--- cassandra.streaming/src/main/java/distribuidos/cassandra/streaming/controllers/PokemonController.java
+++ cassandra.streaming/src/main/java/distribuidos/cassandra/streaming/controllers/PokemonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-
+@CrossOrigin
 @RequestMapping(value = "/pokemones")
 public class PokemonController {
     @Autowired
